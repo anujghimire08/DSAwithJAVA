c1fcfa05ccb2da5a75add89fec1ef7a6a223bb16
Anuj Ghimire <anujghimire08@gmail.com>
2025-12-28 00:04:30 +0545

    List Traverse and Displaying

---
+++ src/LinkedList/DisplayList.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class DisplayList {
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+        Node n4 = new Node(40);
+        Node n5 = new Node(50);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        display(n1);
+    }
+}
