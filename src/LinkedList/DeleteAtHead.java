a59f3a39cfdc9f7254b36b63e2a0a87de4f71694
Anuj Ghimire <contact.anujghimire@gmail.com>
2025-12-30 12:07:27 +0545

    Fixed case for single node and count ll size

--- src/LinkedList/DeleteAtHead.java
+++ src/LinkedList/DeleteAtHead.java
@@ -2,6 +2,7 @@ package LinkedList;
 
 public class DeleteAtHead {
     static Node head, tail;
+    static int llSize;
 
     static void atTail(int val) {
         Node newNode = new Node(val);
@@ -11,13 +12,17 @@ public class DeleteAtHead {
             tail.next = newNode;
             tail = newNode;
         }
+        llSize++;
     }
 
     static void deleteHead() {
         if (head == null) {
             System.out.println("empty list");
+            return;
         }
         head = head.next;
+        if (head == null) tail = null; // for when list has only one node
+        llSize--;
     }
 
     static void display() {
@@ -36,9 +41,11 @@ public class DeleteAtHead {
         atTail(2);
         atTail(3);
         System.out.println("Before Deletion");
+        System.out.println("Linked List Size: " + llSize);
         display();
         deleteHead();
         System.out.println("After Deletion");
         display();
+        System.out.println("Linked List Size: " + llSize);
     }
 }
