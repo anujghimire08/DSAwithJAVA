5f695dad2b1cd0870c6e929ba159bfc8c7281fcd
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-03 10:07:04 +0545

    circular singly ll deletion of first node

---
+++ src/LinkedList/CircularLinkedList/DeleteFirstNode.java
@@ -0,0 +1,51 @@
+package LinkedList.CircularLinkedList;
+
+public class DeleteFirstNode {
+    static Node head, tail;
+
+    static void atHead(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            newNode.next = newNode;
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+            tail.next = newNode;
+        }
+
+    }
+
+    static void deletionAtHead() {
+        if (head == null) return;
+        if (head == tail) {
+            head = tail = null;
+            return;
+        }
+        head = head.next;
+        tail.next = head;
+    }
+
+    static void display() {
+        if (head == null) return;
+        Node temp = head;
+        do {
+            System.out.print(temp.val + "  ");
+            temp = temp.next;
+        } while (temp != head);
+
+    }
+
+
+    public static void main(String[] args) {
+        atHead(10);
+        atHead(20);
+        atHead(30);
+        atHead(40);
+        atHead(50);
+        display();
+        deletionAtHead();
+        System.out.println();
+        display();
+    }
+}
