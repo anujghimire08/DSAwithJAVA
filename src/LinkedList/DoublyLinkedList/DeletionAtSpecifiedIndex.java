621c980e5528fe37998c444b948227d5fda17c92
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-02-02 20:21:38 +0545

    deletion at specified position DOUBLY LL

---
+++ src/LinkedList/DoublyLinkedList/DeletionAtSpecifiedIndex.java
@@ -0,0 +1,85 @@
+package LinkedList.DoublyLinkedList;
+
+public class DeletionAtSpecifiedIndex {
+    static DNode head, tail;
+    static int size;
+
+    static void atTail(int val) {
+        DNode newNode = new DNode(val);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    static void deleteHead() {
+        if (head == null) return;
+        if (head == tail) {
+            head = tail = null;
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+
+        size--;
+    }
+
+    static void deleteTail() {
+        if (head == null) return;
+
+        if (head == tail) { // only one node
+            head = tail = null;
+            return;
+        }
+
+        tail = tail.prev;
+        tail.next = null;
+
+        size--;
+    }
+
+    static void deleteAtSpecifiedIndex(int idx) {
+        if (idx < 0 || idx > size || head == null) return;
+        if (idx == 0) {
+            deleteHead();
+            return;
+        } else if (idx == size) {
+            deleteTail();
+            return;
+        } else {
+            DNode temp = head;
+            for (int i = 0; i < idx - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next.next.prev = temp;
+            temp.next = temp.next.next;
+
+            if (temp.next.next == null) tail = temp;
+        }
+        size--;
+    }
+
+    static void display() {
+        DNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ⇌ ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        atTail(10);
+        atTail(20);
+        atTail(30);
+        atTail(40);
+        atTail(50);
+        display();
+        deleteAtSpecifiedIndex(2);
+        display();
+    }
+}
