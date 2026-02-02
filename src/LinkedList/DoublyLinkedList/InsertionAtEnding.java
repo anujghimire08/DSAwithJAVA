2fafbfd0aac08576347c63c4ef8794b1cc6453d0
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-02-02 17:44:33 +0545

    insertion at tail double linked list

---
+++ src/LinkedList/DoublyLinkedList/InsertionAtEnding.java
@@ -0,0 +1,34 @@
+package LinkedList.DoublyLinkedList;
+
+public class InsertionAtEnding {
+    static DNode head, tail;
+
+    static void atTail(int val) {
+        DNode newNode = new DNode(val);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    static void display() {
+        DNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ⇌ ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        atTail(10);
+        atTail(20);
+        atTail(30);
+        display();
+        atTail(40);
+        display();
+    }
+}
