9bffd95d695750f1407006123e89a97337cb7f09
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-02-23 08:45:12 +0545

    Stack via LinkedList

---
+++ src/Stack/LinkedListStack.java
@@ -0,0 +1,68 @@
+package Stack;
+
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class LinkedListStack {
+    static Node head;
+    static int size;
+
+    static void push(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    static int peek() {
+        if (head == null) {
+            throw new IllegalStateException("Stack Underflow");
+        }
+        return head.val;
+    }
+
+    static int pop() {
+        if (head == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        int x = head.val;
+        head = head.next;
+        size--;
+        return x;
+    }
+
+    static int stackSize() {
+        return size;
+    }
+
+    static void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(pop());
+        push(10);
+        push(20);
+        push(30);
+        push(40);
+        display();
+        System.out.println(pop() + " popped");
+        display();
+        push(40);
+        display();
+        System.out.println(stackSize() + " stack size");
+        System.out.println();
+        System.out.println(peek());
+    }
+}
