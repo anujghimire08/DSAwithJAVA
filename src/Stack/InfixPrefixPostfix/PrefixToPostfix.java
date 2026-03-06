45416387befabca05b5a7c1addb63ed2a08e0347
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-06 16:26:01 +0545

    PrefixToPostfix

--- src/Stack/InfixPrefixPostfix/PrefixToPostfix.java
+++ src/Stack/InfixPrefixPostfix/PrefixToPostfix.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class PrefixToPostfix {
     public static void main(String[] args) {
         Stack<String> postfix = new Stack<>();
-        String prefix = "/AB*+DEF";
+        String prefix = "/-AB*+DEF";
         for (int i = prefix.length() - 1; i >= 0; i--) {
             char ch = prefix.charAt(i);
             if (Character.isLetterOrDigit(ch)) {
