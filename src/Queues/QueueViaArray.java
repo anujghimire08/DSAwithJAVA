983530b45ee52879398044d96996e66b3e16107d
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-17 23:20:19 +0545

    Queue via Linear Array

---
+++ src/Queues/QueueViaArray.java
@@ -0,0 +1,76 @@
+package Queues;
+
+public class QueueViaArray {
+    private int rear, front, size;
+    private int[] arr;
+
+    QueueViaArray(int capacity) {
+        arr = new int[capacity];
+    }
+    void add(int val) {
+        if (size >= arr.length) {
+            System.out.println("Queue Overflow");
+            return;
+        }
+        arr[rear++] = val;
+        size++;
+    }
+    int remove() {
+        if (size == 0) {
+            System.out.println("Queue Empty");
+            return -1;
+        }
+        int frontVal = arr[front];
+        front++;
+        size--;
+        return frontVal;
+    }
+    int peek() {
+        if (size == 0) {
+            System.out.println("Queue Empty");
+            return -1;
+        }
+        return arr[front];
+
+    }
+    boolean isEmpty() {
+        return size == 0;
+    }
+    boolean isFull() {
+        return size == arr.length;
+    }
+    void display() {
+        if (isEmpty()) {
+            System.out.println("Queue Empty");
+        } else {
+            for (int i = front; i < rear; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        QueueViaArray qa = new QueueViaArray(5);
+        System.out.println(qa.isEmpty());
+        qa.add(10);
+        qa.add(20);
+        qa.add(30);
+        qa.add(40);
+        qa.add(50);
+        qa.display();
+        qa.remove();
+        qa.display();
+        System.out.println("top value: " + qa.peek());
+        System.out.println(qa.isEmpty());
+        System.out.println(qa.isFull());
+    }
+}
+/*
+| Issue                    | Explanation                         |
+| ------------------------ | ----------------------------------- |
+| ❌ Space waste            | Removed elements leave unused space |
+| ❌ Cannot reuse array     | `rear` only increases               |
+| ❌ Possible index error   | `rear` exceeds array length         |
+| ❌ Inefficient memory use | Even when queue has space           |
+*/
