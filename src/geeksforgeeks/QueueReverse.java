23531c59b425ebea4d625196d8d318c3c6c6b30c
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-17 10:10:35 +0545

    geeksforgeeks QUEUE REVERSE

---
+++ src/geeksforgeeks/QueueReverse.java
@@ -0,0 +1,17 @@
+package geeksforgeeks;
+//Given a queue q containing integer elements, your task is to reverse the queue.
+public class QueueReverse {
+/**
+ class Solution {
+ public void reverseQueue(Queue<Integer> q) {
+ Stack<Integer> st = new Stack<>();
+ while(!q.isEmpty()){
+ st.push(q.poll());
+ }
+ while(!st.isEmpty()){
+ q.add(st.pop());
+ }
+ }
+ }
+ */
+}
