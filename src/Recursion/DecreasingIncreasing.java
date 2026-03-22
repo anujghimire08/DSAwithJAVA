a1cd2b70e881ca5bba94e9b3036892dffb53827e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-22 23:10:31 +0545

    DecrementIncrement

---
+++ src/Recursion/DecreasingIncreasing.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class DecreasingIncreasing {
+    static void print(int n) {
+        if (n == 0) return;
+        System.out.print(n);
+        print(n - 1);
+        System.out.print(n);
+    }
+
+    public static void main(String[] args) {
+        print(4);
+    }
+
+}
