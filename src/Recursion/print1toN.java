ad79d7c28ec728d02dcffa40e918548f46d0ac37
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-22 22:11:19 +0545

    print 1 to N

---
+++ src/Recursion/print1toN.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class print1toN {
+    static void print(int x, int n) {
+        if (x > n) return;
+        System.out.println(x);
+        print(x + 1, n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter nth value:");
+        int n = sc.nextInt();
+        print(1, n);
+    }
+}
