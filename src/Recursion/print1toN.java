acd598bfaa7422ba364afa938d9addc20b68fd5a
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-22 23:01:54 +0545

    1 to N using reursion callback method

--- src/Recursion/print1toN.java
+++ src/Recursion/print1toN.java
@@ -9,10 +9,22 @@ public class print1toN {
         print(x + 1, n);
     }
 
+    static void print(int x) {
+        if (x == 0) return;
+        print(x - 1);
+        System.out.println(x);
+    }
+
+    //    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter nth value:");
+//        int n = sc.nextInt();
+//        print(1, n);
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter nth value:");
         int n = sc.nextInt();
-        print(1, n);
+        print(n);
     }
 }
