80a50bcae1bff1dc566d2a898b8b476df84919a7
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-27 22:42:07 +0545

    Added Step functionality using left shift operator

--- src/Recursion/HanoiTower.java
+++ src/Recursion/HanoiTower.java
@@ -18,7 +18,7 @@ public class HanoiTower {
     }
 
     private static void Steps(int n) {
-        int step = (int) Math.pow(2, n) - 1;
+        int step = (2 << n) - 1;
         System.out.println("Total Step for " + n + " disks is:" + step);
     }
 }
