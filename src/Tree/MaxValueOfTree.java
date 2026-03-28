a7de2862bac46724436c56b6bc38a26c384c0868
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-28 22:53:05 +0545

    Maximum Value of a B.Tree

---
+++ src/Tree/MaxValueOfTree.java
@@ -0,0 +1,29 @@
+package Tree;
+
+public class MaxValueOfTree {
+    public static void main(String[] args) {
+//            1
+//          /   \
+//         2     3
+//        /  \  /  \
+//       4   5  6   7
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        a.left = b;
+        b.left = d;
+        b.right = e;
+        a.right = c;
+        c.left = f;
+        c.right = g;
+        System.out.println("Max Value of a Tree : " + Max(a));
+    }
+
+    static int Max(Node root) {
+        return (root == null) ? Integer.MIN_VALUE : Math.max(root.val, Math.max(Max(root.left), Max(root.right)));
+    }
+}
