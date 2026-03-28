c7eb226c917426c024c28173a3061a4c7c458181
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-28 17:25:50 +0545

    created a binary tree manually

---
+++ src/Tree/CreationOfTree.java
@@ -0,0 +1,33 @@
+package Tree;
+
+class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class CreationOfTree {
+    public static void main(String[] args) {
+//            1
+//          /   \
+//         2     3
+//        /  \  /  \
+//       4   5  6   7
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        a.left = b;
+        b.left = d;
+        b.right = e;
+        a.right = c;
+        c.left = f;
+        c.right = g;
+    }
+}
