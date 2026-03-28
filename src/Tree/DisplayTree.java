1e806a5d962a36e420afa1daab1d22f69aac7643
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-03-28 17:32:43 +0545

    displaying  binary tree using preorder traversal

---
+++ src/Tree/DisplayTree.java
@@ -0,0 +1,31 @@
+package Tree;
+public class DisplayTree {
+    public static void main(String[] args) {
+//            1
+//          /   \
+//         2     3
+//        /  \  /  \
+//       4   5  6   7
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        a.left = b;
+        b.left = d;
+        b.right = e;
+        a.right = c;
+        c.left = f;
+        c.right = g;
+        display(a);
+    }
+
+    static void display(Node root) {
+        if (root == null) return;
+        System.out.print(root.val + " ");
+        display(root.left);
+        display(root.right);
+    }
+}
