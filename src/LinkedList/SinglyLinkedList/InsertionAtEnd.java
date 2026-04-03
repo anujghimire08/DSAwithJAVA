8409ee0511dca5775e3787cc8f0944edcbd89faa
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-03 19:31:52 +0545

    InsertionAtEnd SLL

---
+++ src/LinkedList/SinglyLinkedList/InsertionAtEnd.java
@@ -0,0 +1,35 @@
+package LinkedList.SinglyLinkedList;
+
+public class InsertionAtEnd {
+    static Node head, tail;
+
+    static void atTail(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+
+    static void display() {
+        if (head == null) return;
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+        atTail(1);
+        atTail(2);
+        atTail(3);
+        atTail(4);
+        atTail(5);
+        display();
+    }
+}
