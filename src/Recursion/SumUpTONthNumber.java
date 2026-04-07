3592d51240e2ec3ed924b4ed496ec3d2b0eae94e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-07 08:38:04 +0545

    Lab 14

---
+++ src/Recursion/SumUpTONthNumber.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class SumUpTONthNumber {
+    static int sum(int num) {
+        if (num == 0) return 0;
+        return num + sum(num - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
