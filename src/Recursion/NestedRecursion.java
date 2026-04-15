69eb09a4fc8fa05c14fdb52e551c9401c481e097
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-04-15 21:20:31 +0545

    countDigitSum Nested Recursuion

--- src/Recursion/NestedRecursion.java
+++ src/Recursion/NestedRecursion.java
@@ -3,12 +3,12 @@ package Recursion;
 public class NestedRecursion {
     public static void main(String[] args) {
         int n = 123;
-        System.out.println(countDigit(n));
+        System.out.println(countDigitSum(n));
     }
 
     // Function parameter itself is a recursive call
-    private static int countDigit(int n) {
-        return (n < 10) ? n : countDigit(countDigit(n / 10) + n % 10);
+    private static int countDigitSum(int n) {
+        return (n < 10) ? n : countDigitSum(countDigitSum(n / 10) + n % 10);
     }
 
 }
